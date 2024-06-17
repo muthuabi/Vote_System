b3c63e33913843ca445f5dec6aa75d100f4f6e3e
muthuabi <muthuabi292@gmail.com>
2024-06-17 10:32:31 +0530

    New Brach Commit with Various Admin Option

---
+++ assets/css-js/script.js
@@ -0,0 +1,91 @@
+const maincanvas=document.querySelector("#maincanvas");
+window.addEventListener("resize",(e)=>{
+    if(maincanvas)
+    {
+       if(window.innerWidth>=768)
+       {
+           //maincanvas.style.transform='translateX(0)';
+           maincanvas.style.visibility='visible';
+           document.querySelectorAll('.modal-backdrop').forEach(element=>{
+            element.style.display='none';
+           })   
+        //    maincanvas.setAttribute('visibility','visible');
+       }
+       else
+       { 
+           maincanvas.style.visibility='hidden';
+       }
+    }
+       
+})
+function print_doc(id='')
+{
+    if(!id)
+    {
+        document.querySelectorAll(`#ballot_all tr:has(#vote_status[class='down_vote'])`).forEach(element=>{
+            element.style.display='none';
+        })
+    }
+    else
+    {
+        document.body.innerHTM=document.querySelector(`#${id}`).innerHTML;
+      
+    }
+    window.print();
+    location.reload();
+}
+document.addEventListener("DOMContentLoaded",(event)=>{
+    const date=new Date();
+    const academic_year=date.getFullYear()+'-'+((date.getFullYear()+1)%100);
+    const sxc_footer_head=`Designed & Maintained by SXC ERP and Web Team | © 2024 St. Xavier's College. All rights reserved`;
+    const sxc_heading='Students Council Election '+academic_year;
+    const on_off=document.getElementById('on_off');
+    const liveblink=document.getElementById('live-blink');
+    const votestatus=document.getElementById('vote_status');
+    const footerhead=document.querySelector('.footer-head b');
+    const sxcheader=document.querySelector('.sxc-council-header h5 ');
+    const ballothead=document.querySelector('#ballot_head b');
+    const tab_title=document.querySelector('head title');
+    let tab_icon=document.querySelector(`link[rel="shortcut icon"]`);
+    const head=document.querySelector('head');
+    if(!tab_icon)
+    {
+        if(head)
+        {
+           head.innerHTML+=`<link rel='shortcut icon' href='http://localhost/Vote_System/assets/images/other_images/logo2.png' type='image/x-icon' /> `;
+        }
+    }
+    if(tab_title)
+        tab_title.innerHTML=sxc_heading;
+    if(footerhead)
+    footerhead.innerHTML=sxc_footer_head;
+	if(sxcheader)
+    sxcheader.innerHTML=sxc_heading;
+    if(ballothead)
+    ballothead.innerHTML=sxc_heading; 
+    function init_toast(message = "", id = "my_toast_offline") {
+    const toast = document.querySelector(`.toast#${id}`);
+    if (message)
+      document.querySelector(`#${id} #message`).innerText = message;
+    toaster = new bootstrap.Toast(toast);
+    toaster.show();
+  }
+    window.addEventListener("online",(e)=>{
+        if(on_off)
+        on_off.innerHTML=`<small style='background-color:green;border-radius:50%;width:15px;height:15px;'></small><small>Online</small>`;
+        if(liveblink)
+        liveblink.getAnimations()[0].play();
+        // votestatus.getAnimations()[0].play();
+		init_toast('Connection Back Online');
+	})
+    window.addEventListener("offline",(e)=>{
+        if(on_off)
+        on_off.innerHTML=`<small style='background-color:red;border-radius:50%;width:15px;height:15px;'></small><small>Offline</small>`;
+		if(liveblink)
+        liveblink.getAnimations()[0].pause();
+		// votestatus.getAnimations()[0].pause();
+        init_toast('Connection Offline');
+	})
+    
+})
+window.history.replaceState(null,null,window.location.href);
---
+++ connection/connection.php
@@ -0,0 +1,37 @@
+<?php
+class Connection
+{
+    private $db_host='localhost';
+    private $db_user='root';
+    private $db_password='';
+    private $db_dbase='sxc_election';
+    protected $conn;
+    public function __construct()
+    {
+        try{
+        $this->conn=new mysqli($this->db_host,$this->db_user,$this->db_password,$this->db_dbase);
+        if($this->conn->connect_error)
+            throw new Exception('Database Connection Failed');
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+            die('Database Connection Failed');
+        }
+
+    }
+    public function initConnection()
+    {
+        return $this->conn;
+    }
+    public function closeConnection()
+    {
+        $this->conn->close();
+        return $this->conn;
+    }
+
+}
+$db=new Connection();
+$conn=$db->initConnection();
+define('ROOT_DIR','SXC_VOTE_SYSTEM/');
+?>
