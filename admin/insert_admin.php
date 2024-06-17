a1a6761052f8dc9b1a3be2949bb09f6382582d7b
muthuabi <muthuabi292@gmail.com>
2024-06-17 10:24:43 +0530

    check

--- admin/insert_admin.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    include_once('../util_classes/Admin.php');
-    if($admin->insert_admin('krishnan','Muthukrishnan',base64_encode('12345'),'muthukrish@gmail.com'))
-    {
-        echo "Admin Created";
-    }
-?>
